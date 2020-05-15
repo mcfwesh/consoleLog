a3418ed99082027ee33b5897c634254e7d7116cd
dionisio ugalde <dionisiougalde@iMac-de-dionisio-8.local>
2020-05-15 10:43:56 +0200

    friday

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -1,37 +1,12 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import MonacoEditor from "react-monaco-editor";
-//const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-// export default class Notes extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <h2>Extra notes From Ironhack</h2>
-//         <textarea style={{ width: "1000px", height: "2000px" }}></textarea>
-//       </div>
-//     );
-//   }
-// }
-// module.exports = {
-//   entry: "./index.js",
-//   output: {
-//     path: path.resolve(__dirname, "dist"),
-//     filename: "app.js",
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: ["style-loader", "css-loader"],
-//       },
-//       {
-//         test: /\.ttf$/,
-//         use: ["file-loader"],
-//       },
-//     ],
-//   },
-//   plugins: [new MonacoWebpackPlugin()],
-// };
+// import * as monaco from "monaco-editor";
+
+// monaco.editor.create(document.getElementById("container"), {
+//   value: 'console.log("Hello, world")',
+//   language: "javascript",
+// });
 
 class Notes extends React.Component {
   constructor(props) {
@@ -53,16 +28,20 @@ class Notes extends React.Component {
       selectOnLineNumbers: true,
     };
     return (
-      <MonacoEditor
-        width="80%"
-        height="1000"
-        language="javascript"
-        theme="vs-dark"
-        value={code}
-        options={options}
-        onChange={this.onChange}
-        editorDidMount={this.editorDidMount}
-      />
+      <div>
+        <br></br>
+        <MonacoEditor
+          class="margin-auto"
+          width="80%"
+          height="800"
+          language="javascript"
+          theme="vs-dark"
+          value={code}
+          options={options}
+          onChange={this.onChange}
+          editorDidMount={this.editorDidMount}
+        />
+      </div>
     );
   }
 }
