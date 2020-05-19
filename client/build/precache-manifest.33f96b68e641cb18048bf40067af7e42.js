efd74cf0ff0a058ef478327551e50517a1933a4b
HeiseMo <tim.james.tanurhan@gmail.com>
2020-05-20 01:10:24 +0200

    build stuff

---
+++ client/build/precache-manifest.33f96b68e641cb18048bf40067af7e42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f3ab3075675194e3653bbaec225a9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9afbb0312ad54c7f68c0",
+    "url": "/static/css/2.986052d2.chunk.css"
+  },
+  {
+    "revision": "d9595f8f9b6d36645b2d",
+    "url": "/static/css/main.c37c3faf.chunk.css"
+  },
+  {
+    "revision": "9afbb0312ad54c7f68c0",
+    "url": "/static/js/2.7e2f947a.chunk.js"
+  },
+  {
+    "revision": "8903eea611d90483196a9db6c934dd18",
+    "url": "/static/js/2.7e2f947a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9595f8f9b6d36645b2d",
+    "url": "/static/js/main.80e62447.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
