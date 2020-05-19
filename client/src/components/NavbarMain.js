7ee136f1ddd8a8c1145632de6f14439ea4463102
Nate <mcfwesh@gmail.com>
2020-05-19 11:42:13 +0200

    link bugs

--- client/src/components/NavbarMain.js
+++ client/src/components/NavbarMain.js
@@ -1,28 +1,36 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default class NavbarMain extends Component {
-
   render() {
     return (
-    <div className='navbarMain'>
-    <div className='navleft'>
-      <div className="logo">
-        <img src="https://i.ibb.co/wpQGhGG/Conosole-log-logo.png"/>
-      </div>
-    </div>
-    <div className="navRight">
-    <div className="courses">
-        <div>
-          <Link onClick={this.props.handleCourse} id="Web Dev" to='/panel'>Web Dev</Link>
+      <div className="navbarMain">
+        <div className="navleft">
+          <div className="logo">
+            <Link to="/">
+              <img src="https://i.ibb.co/wpQGhGG/Conosole-log-logo.png" />
+            </Link>
+          </div>
         </div>
-        <div>
-          <Link onClick={this.props.handleCourse} id="UX/UI" to='/panel'>UX/UI</Link>
-        </div>
-        <div>
-          <Link onClick={this.props.handleCourse} id="Data" to='/panel'>Data</Link>
+        <div className="navRight">
+          <div className="courses">
+            <div>
+              <Link onClick={this.props.handleCourse} id="Web Dev" to="/panel">
+                Web Dev
+              </Link>
+            </div>
+            <div>
+              <Link onClick={this.props.handleCourse} id="UX/UI" to="/panel">
+                UX/UI
+              </Link>
+            </div>
+            <div>
+              <Link onClick={this.props.handleCourse} id="Data" to="/panel">
+                Data
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-    </div>
-    )}
+    );
+  }
 }
